60f287250bbd2fd214541e019d2c10c27cc84aa5
Ruma Choudhury <rumachoudhury7862@gmail.com>
2025-09-06 01:38:30 -0400

    Prepare for the deplyment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,16 +4,26 @@ import messageRoutes from "./routes/messageRoutes.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+// const _dirname = path.resolve();
+
+// Recreate __dirname in ES Module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 
 // Middleware to parse JSON bodies
 app.use(express.json());
@@ -21,6 +31,14 @@ app.use(express.json());
 app.use(rateLimiter);
 
 app.use("/api/message", messageRoutes);
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+if (process.env.NODE_ENV === "production") {
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 //First connect to DB then start the server and this is good practice because if DB connection fails server will not start
 connectDB().then(() => {
